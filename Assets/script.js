9aedebccc81839966e1f37258be118df11a306d9
PaulRusz <paulruszkay@gmail.com>
2024-02-12 11:20:35 -0500

    wrote a lot of code for JS that does not work

--- Assets/script.js
+++ Assets/script.js
@@ -1,15 +1,6 @@
-/*  
-function generateQuiz(questions, quizContainer, resultsContainer, submitButon) {
-    function showQuestions(questions, quizContainer) {
-
-    }
-}
-*/
-
-
-var testQuestions = [    
+ var testQuestions = [    
     {
-        prompt: "What does HTML stand for?",
+        question: "What does HTML stand for?",
         options: [
             "Hyper Text Markup Language",
             "Home Text Managment Link",
@@ -19,7 +10,7 @@ var testQuestions = [
     },
 
     {
-        prompt: "What is the universal selector for CSS?",
+        question: "What is the universal selector for CSS?",
         options: [
             "#",
             ".",
@@ -29,16 +20,23 @@ var testQuestions = [
     }
 ]
 
-let timerEl = document.querySelector("timer")
+let quizContainer = document.querySelector("container")
+let timerEl = document.querySelector("time")
 let startButton = document.querySelector("start")
-let questionsEl = document.querySelector("testQuestions")
+let questionsEl = document.querySelector("question")
+let optionsEl = document.querySelector("options")
+let answerEl = document.querySelector("answer")
 
 let time = testQuestions.length * 10;
 let timerID;
 
+let currentQuestion = 0;
+let score = 0;
+let incorrectAnswer = 0;
 
 
-function timerClock() {
+
+/*function timerClock() {
     time--;
     timerEl.textContent = time;
     if (time <= 0) {
@@ -46,22 +44,95 @@ function timerClock() {
     }
     timerClock()
 }
+*/
 
 
-
-
+/*
 function start() {
-    timerID = setinterval(clockTick, 1500)
-    timerEl.textContent = time;
-
     let startScreenEl = 
-    document.getElementById("start")
+    document.getElementById("start-screen")
     startScreenEl.setAttribute ("class", "hide")
     questionsEl.removeAttribute("class")
-    start()
+    startQuestions()
 }
-startButton.onclick = start;
+
+function startQuestions() {
+    let currentQuestion = question[currentQuestion];
+    let quesEl = document.getElementById("ques")
+
+    quesEl.textContent = currentQuestion.question;
+    optionsEl = ""
+    currentQuestion.options.forEach(
+        function (choice, i) {
+            let choiceButton = document.createElement("button");
+            choiceButton.setAttribute("value", choice);
+            choiceButton.textContent = i + 1 + ". " + choice;
+            choiceButton.onclick = questionClick;
+            optionsEl.appendChild(choiceButton)
+        }
+    )
+}
+*/
 
 
 
 
+
+/*function showQuestion() {
+    const question = quizData[currentQuestion];
+    questionElement.innerText = question.question;
+  
+    optionsElement.innerHTML = "";
+    question.options.forEach(option => {
+      const button = document.createElement("button");
+      button.innerText = option;
+      optionsElement.appendChild(button);
+      button.addEventListener("click", selectAnswer);
+    });
+  }
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+function startQuiz() {
+    var testData = testQuestions[currentQuestion];
+    questionsEl = testData.testData;
+
+    optionsEl = ""
+    testData.options.forEach(option =>  
+    {
+        var button = document.createElement("button");
+        button.innerText = option;
+        optionsEl.appendChild(button);
+        button.addEventListener("click", selectAnswer);
+    });
+}
+
+function selectAnswer() {
+    var selectedButton = target;
+    var answer = testQuestions[currentQuestion].answer;
+    
+    if (selectedButton === answer) {
+        score++;
+    }
+
+    currentQuestion++;
+
+    if (currentQuestion < testQuestions.length) {
+        startQuiz();
+} }
+
+startQuiz();
+*/
